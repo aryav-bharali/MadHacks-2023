484095366f7e1b05381b5c6535cc7e76b13565db
Aryav Bharali <bharali@wisc.edu>
2023-11-12 03:30:40 -0600

    Cleaned LLM Output

--- src/app/project/ParsedResponse.tsx
+++ src/app/project/ParsedResponse.tsx
@@ -4,12 +4,22 @@ interface PropTypes {
   feedback: string
 }
 
+const truncateString = (str: string) =>
+  str.substring(0, str.lastIndexOf('. ') + 1)
+const countWords = (str: string) =>
+  str.split(' ').filter((item) => item !== '').length
+
 function ParsedResponse({ feedback }: PropTypes) {
   if (feedback.indexOf('"') == 0) feedback = feedback.substring(1)
   if (feedback.charAt(feedback.length - 1) === '"')
     feedback = feedback.substring(0, feedback.length - 1)
   feedback = feedback.replaceAll('\\n\\n', '\n\n').replaceAll('\\n*', '')
-  feedback = feedback.substring(0, feedback.lastIndexOf('. ') + 1)
+  if (
+    feedback.substring(0, feedback.lastIndexOf('. ') + 1).indexOf('. ') !== -1
+  ) {
+    feedback = truncateString(feedback)
+    while (countWords(feedback) > 175) feedback = truncateString(feedback)
+  }
 
   return <Markdown>{feedback}</Markdown>
 }
